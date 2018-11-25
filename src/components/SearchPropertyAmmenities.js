f4f4337712aa49a79db954daf7002272bc3cbc27
Andrew Watters <Andrewdwatters1@gmail.com>
2018-11-24 17:26:32 -0700

    layed out tasks w/ trello, finish query construction then send to backend for prep and send to datafiniti

--- src/components/SearchPropertyAmmenities.js
+++ src/components/SearchPropertyAmmenities.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import { runInThisContext } from 'vm';
 
 class SearchPropertyAmmenities extends Component {
   constructor() {
@@ -15,33 +14,13 @@ class SearchPropertyAmmenities extends Component {
     }
   }
 
-  setbedsMin = (e) => {
-    const { name, value } = e.target;
-    const max = document.querySelector('#bedrooms-max');
-    console.log(max);
-    // if value === 3 => max innerHTML = 4, 5
-    // else if value === 1 => max inner HTML = 2, 3, 4, 5
-    this.setState({
-      bedsMin: value
-    })
-  }
-
-  // setBedroomsMax = (e) => {
-  //   this.setState({
-  //     bedRoomsMax: e.target.value
-  //   })
-  // }
   handleBedsChange = (e) => {
     const { name, value } = e.target;
     const index = this.state.bedsPossValues.findIndex(e => e === value);
     const valueToSet = `${name}Index`;
-    console.log(valueToSet, ': ', index);
     this.setState({
       [name]: value,
       [valueToSet]: index
-    }, () => {
-      console.log(this.state.bedsMinIndex);
-      console.log(this.state.bedsMaxIndex);
     })
   }
 
@@ -69,7 +48,31 @@ class SearchPropertyAmmenities extends Component {
               .filter((e, i) => this.state.bedsMinIndex > -1 ? i > this.state.bedsMinIndex : e)
               .map((elem, index) => <option key={index} value={elem}>{elem}</option>)}
           </select>
+        </div>
 
+
+        <div>
+          {/* BATHROOMS */}
+        </div>
+
+
+        <div>
+          {/* SQUARE FEET */}
+        </div>
+
+
+        <div>
+          {/* LOT SIZE */}
+        </div>
+
+
+        <div>
+          {/* PARKING */}
+        </div>
+
+
+        <div>
+          {/* PETS */}
         </div>
 
 
